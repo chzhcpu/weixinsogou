098acacd42b6843e1890935fba70b2a9e77110bc
chzhcpu <chzhcpu@163.com>
2015-11-02 08:50:47 +0800

    首次上传

---
+++ GzhSearcher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace WeiXinSogou
+{
+    class GzhSearcher
+    {
+        public string SearchServerUrl = "http://weixin.sogou.com/weixin?type=1&query={0}&ie=utf8";
+        public List<Gzh> Search(string sterm)
+        {
+            string surl = string.Format(SearchServerUrl, sterm);
+            HtmlWeb htmlweb = new HtmlWeb();
+            HtmlDocument htmldoc = htmlweb.Load(surl);
+
+            HtmlNodeCollection gzhNodeCol = htmldoc.DocumentNode.SelectNodes(".//div[@class='wx-rb bg-blue wx-rb_v1 _item']");
+
+            List<Gzh> gzhCol = new List<Gzh>();
+            foreach (HtmlNode gzhNode in gzhNodeCol)
+            {
+                Gzh gzh=new Gzh();
+                gzh.GzhUrl = gzhNode.GetAttributeValue("href", "").Replace("&amp;", "&");
+                gzh.GzhName= gzhNode.SelectSingleNode("./div/h3").InnerText;
+                gzh.WxhName= gzhNode.SelectSingleNode("./div/h4").InnerText;
+                gzhCol.Add(gzh);
+            }
+
+            return gzhCol;
+        }
+    }
+
+    class Gzh
+    {
+        public string GzhUrl { get; set; }
+        public string GzhName { get; set; }
+        public string WxhName { get; set; }
+
+        public string Descrip { get; set; }
+        public string AuthCorp { get; set; }
+        public string ImageFileName { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeiXinSogou
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GzhSearcher gzhs = new GzhSearcher();
+            List<Gzh> list= gzhs.Search("识林");
+            Console.Read();
+        }
+    }
+}
